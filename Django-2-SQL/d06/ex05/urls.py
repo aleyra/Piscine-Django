718310c17baeb2cadd48d0617009157ccc10f289
Lucille Burnet <lburnet@z3r4p4.42lyon.fr>
2023-12-13 15:54:10 +0100

    2j pour max 100XP grr

--- Django-2-SQL/d06/ex05/urls.py
+++ Django-2-SQL/d06/ex05/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from . import views
-from ex03 import views as views3
 
 urlpatterns = [
-    path("populate/", views3.populate, name="populate"),
-    path("display/", views3.display, name="display")
+    path("populate/", views.populate, name="populate"),
+    path("display/", views.display, name="display"),
+    path("remove/", views.remove, name="remove"),
 ]
--- Django-2-SQL/d06/ex05/views.py
+++ Django-2-SQL/d06/ex05/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+from d06.tools import ex_name
+from ex02.views import movies
+from ex05.models import Movies
+from ex04.form import RemoveForm
 
 # Create your views here.
+
+def populate(request):
+    response = ""
+    for movie in movies:
+        try:
+            row = Movies(**movie)
+            row.save()
+            response += f"{movie['title']}: saved<br \>"
+        except Exception as err:
+            response += f"{movie['title']}: {err}<br \>"
+    return HttpResponse(response)
+
+def display(request):
+    ex_nb = ex_name(request)
+
+    try:
+        movies_lst = Movies.objects.all().values()
+        print(f"\n\n{movies_lst}\n\n")  #
+    except Exception as err:
+        return HttpResponse(f"No data available because: {err}")
+    return render(request, f"{ex_nb}/table.html", {'movies_lst': movies_lst})
+
+def remove(request):
+    ex_nb = ex_name(request)
+
+    if request.method == 'GET':
+        movies_lst = Movies.objects.all()
+        # print(movies_lst)  #
+        if len(movies_lst) == 0:
+            return HttpResponse("No data available")
+        context = {'form': RemoveForm(choices=((movie.title, movie.title) for movie in movies_lst))}
+        return render(request, f"{ex_nb}/remove.html", context)
+    elif request.method == 'POST':
+        title_delete = request.POST.get('title')
+        Movies.objects.get(title=title_delete).delete()
+        return HttpResponseRedirect(request.path)
